1763b9ac1d949b61d68e040384411c9afd8ac915
jams10 <52204522+jams10@users.noreply.github.com>
2020-05-17 14:24:37 +0900

    Update Vector2D.cpp

--- 2D Vector/Vector2D.cpp
+++ 2D Vector/Vector2D.cpp
@@ -1,4 +1,5 @@
 #include "Vector2D.h"
+#include <cmath>
 
 Vector2D::Vector2D( float x_in, float y_in ) : x(x_in), y(y_in)
 {
@@ -28,6 +29,41 @@ Vector2D Vector2D::operator*( float rhs ) const
 Vector2D& Vector2D::operator*=( float rhs )
 {
 	return *this = *this * rhs;
+}
+
+Vector2D Vector2D::operator-( const Vector2D& rhs ) const
+{
+	return Vector2D( x - rhs.x, y - rhs.y );
+}
+
+Vector2D& Vector2D::operator-=( const Vector2D& rhs )
+{
+	return *this = *this - rhs;
+}
+
+Vector2D& Vector2D::Normalize()
+{
+	return *this = GetNormalized();
+}
+
+Vector2D Vector2D::GetNormalized() const
+{
+	const float len = GetLength();
+	if( len != 0.0f ) // Exception : divide by zero.
+	{
+		return *this * (1.0f / len);
+	}
+	return *this;
+}
+
+float Vector2D::GetLengthSq() const
+{
+	return x * x + y * y;
+}
+
+float Vector2D::GetLength() const
+{
+	return std::sqrt( GetLengthSq() );
 }
 
 float Vector2D::GetX()
